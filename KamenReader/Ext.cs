3e1dd36b7225b21b9e5c42cf41c8c0d2f6f300dc
asakura89 <flamboyan3no1a@gmail.com>
2018-01-26 22:10:29 +0700

    [Ext] add clean up empty data [Version]  dump to 1.0.2018.0326

---
+++ KamenReader/Ext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KamenReader
+{
+    public static class Ext
+    {
+        public static IList<GridData> CleanupRawData(this FileReaderResult rawData)
+        {
+            return rawData.Data
+                .GroupBy(prm => prm.Row)
+                .Select(grp => grp.Select(item => item))
+                .Where(grd => !grd.Aggregate(true, (p, n) => p && String.IsNullOrEmpty(n.CellValue)))
+                .SelectMany(grd => grd)
+                .ToList();
+        }
+    }
+}
