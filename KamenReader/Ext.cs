3847e693fa2bb91b06ed09d42103a69b586da095
asakura89 <flamboyan3no1a@gmail.com>
2018-02-18 20:41:45 +0700

    [Ext] improve clean up empty data [Version] 1.0.2018.0418

--- KamenReader/Ext.cs
+++ KamenReader/Ext.cs
@@ -8,12 +8,21 @@ namespace KamenReader
     {
         public static IList<GridData> CleanupRawData(this FileReaderResult rawData)
         {
-            return rawData.Data
-                .GroupBy(prm => prm.Row)
-                .Select(grp => grp.Select(item => item))
-                .Where(grd => !grd.Aggregate(true, (p, n) => p && String.IsNullOrEmpty(n.CellValue)))
-                .SelectMany(grd => grd)
+            IList<GridData> cleanupColumn = rawData.Data;
+            if (rawData.Titles != null && rawData.Titles.Any() && !rawData.Titles.All(ttl => String.IsNullOrEmpty(ttl)))
+            cleanupColumn = rawData
+                .Titles
+                .Select((ttl, idx) => new {Ordinal = idx+1, Item = ttl})
+                .Where(ttl => !String.IsNullOrEmpty(ttl.Item))
+                .Join(rawData.Data, o => o.Ordinal, i => i.Column, (o, i) => i)
                 .ToList();
+    
+        return cleanupColumn
+            .GroupBy(prm => prm.Row)
+            .Select(grp => grp.Select(item => item))
+            .Where(grd => !grd.Aggregate(true, (p, n) => p && String.IsNullOrEmpty(n.CellValue)))
+            .SelectMany(grd => grd)
+            .ToList();
         }
     }
 }
